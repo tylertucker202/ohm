edaf00194cacf1ce2fdf72d40a02318818e1fd5f
tylertucker202 <tylertucker202@gmail.com>
2021-01-08 16:56:16 -1000

    make it better

--- src/app/targets/get-dss-img.service.ts
+++ src/app/targets/get-dss-img.service.ts
@@ -8,6 +8,46 @@ export class GetDssImgService {
 
   constructor(private http: HttpClient) { }
 
+  public construct_url(coords: any): string {
+    /*
+    CGI parameters
+    Here are the CGI parameters currently recognized:
+    r  - right ascension
+    d  - declination
+    e  - equinox (B1950 or J2000; default: J2000)
+    h  - height of image (arcminutes; default: 15.0)
+    w  - width of image (arcminutes; default: 15.0)
+    f  - image format (FITS or GIF; default: FITS)
+    c  - compression (UNIX, GZIP, or NONE; default: NONE; compression 
+        applies to FITS only)
+    v  - Which version of the survey to use:
+        1  - First Generation survey (garden variety)
+        2  - Second generation survey (incomplete)
+        3  - Check the 2nd generation; if no image is available,
+              then go to the 1st generation.
+        4  - The Quick V survey (whence came the Guide Stars Catalog;
+              used mostly for Phase II proposal submission)
+    s  - Save the file to disk instead of trying to display.
+        (ON (or anything) or not defined; default: not defined.)
+
+  Case is not significant in the values, but it is in the parameter names (e.g., use r=, not R=).
+
+  If these are too cryptic, there are synonyms:
+
+    r -> ra
+    d -> dec
+    e -> equinox
+    h -> height
+    w -> width
+    f -> format
+    c -> compression
+    v -> version
+    s -> save
+  */
+  let imageUrl = 'http://archive.stsci.edu/cgi-bin/dss_search'
+  return imageUrl
+  }
+
   public get_dss_img(imageUrl: string): Observable<Blob> {
     return this.http.get(imageUrl, {responseType: 'blob'})
   }
