a965cb19c591ceb182b0f22d73c98b3dce87b232
tylertucker202 <tylertucker202@gmail.com>
2021-01-14 08:59:34 -1000

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-app.use(express.static(__dirname + '/dist/ohm'));
+app.use(express.static(__dirname + 'dist'));
 app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'dist', 'ohm', 'index.html'))
+res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 4200);
